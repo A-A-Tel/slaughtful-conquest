c24df154bd6efad38e7536baf7ce1f68923201ed
A-A-Tel <anouaranthonytel@gmail.com>
2026-06-11 09:39:50 +0200

    added classdiagram

--- Maps/TestMap.cs
+++ Maps/TestMap.cs
@@ -1,13 +1,12 @@
 using Godot;
 
-namespace SlaughtfulConquest.Scripts;
+namespace SlaughtfulConquest.Maps;
 
-public partial class HelloWorld : Node2D
+public partial class TestMap : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello World!");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
